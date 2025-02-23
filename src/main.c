b5b7eb2cb703a6e65a94051283f9e257134adc38
Oleg Fedotov <ol.fed25@gmail.com>
2025-02-23 22:32:26 +0300

    add simple program to send ascii bytes via usart

--- src/main.c
+++ src/main.c
@@ -1,5 +1,13 @@
+#include "util.h"
+#include "usart.h"
+
 int main(void) {
+    usart_init();
+
     while (1) {
-        // Do nothing
+        for (byte i = 0; i <= 255; ++i) {
+            usart_send(i);
+            delay(200);
+        }
     }
 }
