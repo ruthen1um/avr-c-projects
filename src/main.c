ea5c10412a1e3dd98c90ed6310660e6bd258ce66
Oleg Fedotov <ol.fed25@gmail.com>
2025-03-01 12:33:19 +0300

    simple echo program

--- src/main.c
+++ src/main.c
@@ -1,5 +1,13 @@
+#include "usart.h"
+#include <stdio.h>
+
 int main(void) {
+    char string[10];
+
+    usart_init();
     while (1) {
-        // Do nothing
+        byte x = usart_receive();
+        snprintf(string, 10, "Got: %c\n", x);
+        usart_send_bytes((byte*)string);
     }
 }
